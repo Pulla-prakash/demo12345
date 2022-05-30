b4fd6d50a80dbe995f52a500fb5cb90fb409cad4
ChoppariAbhi <106383907+ChoppariAbhi@users.noreply.github.com>
2022-05-30 11:18:05 +0530

    Update JavaSpringDay5WithDataBaseApplication.java

--- src/main/java/com/example/demo/JavaSpringDay5WithDataBaseApplication.java
+++ src/main/java/com/example/demo/JavaSpringDay5WithDataBaseApplication.java
@@ -13,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class JavaSpringDay5WithDataBaseApplication {
 
 	public static void main(String[] args) {
+	
 		SpringApplication.run(JavaSpringDay5WithDataBaseApplication.class, args);
 	}
 
